bcfe48b285c9ce09ddf15bfdeaf86638fee31a4d
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-08-21 13:55:10 -0300

    criação do projeto

---
+++ script.js
@@ -0,0 +1,15 @@
+// criação de nome, idade e senha do usuário //
+let nome = prompt("Digite seu nome: ")
+let idade = parseInt(prompt("Digite sua idade: "))
+let senha = prompt("Digite uma senha: ")
+
+// confirmação de cadastro //
+alert(`Cadastro realizado com sucesso!
+
+Nome: ${nome}
+Idade: ${idade}
+Senha: ${senha}
+
+Faça login para acessar sua conta.`)
+
+// login do usuário //
