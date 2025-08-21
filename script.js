c850bacd25286111f57b205061056cabfa82e74b
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-08-21 14:25:15 -0300

    modificado o codigo

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // criação de nome, idade e senha do usuário //
 let nome = prompt("Digite seu nome: ")
 let idade = parseInt(prompt("Digite sua idade: "))
-let senha = prompt("Digite uma senha: ")
 
-// confirmação de cadastro //
-alert(`Cadastro realizado com sucesso!
+if (idade < 13) {
+    alert("Não foi possível concluir o cadastro. Idade mínima: 13 anos")
+}
+
+else if (idade >= 13) {
+    let senha = prompt("Digite uma senha: ")
+}
+
+alert(`--- Cadastro de Usuário ---
 
 Nome: ${nome}
-Idade: ${idade}
+Idade: ${idade} anos
 Senha: ${senha}
 
-Faça login para acessar sua conta.`)
+---------------------------
 
+Faça login para acessar sua conta.`)
 // login do usuário //
+let confnome = prompt("Digite o nome do seu cadastro: ")
+let confidade = parseInt(prompt("Digite a idade do seu cadastro: "))
+let confsenha = prompt("Digite a senha do seu cadastro: ")
