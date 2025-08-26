09cbaa61aaa003ecdb883fec61c97f7c01700eb6
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-08-26 10:54:31 -0300

    modificado confirmação do Js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // criação de nome, idade e senha do usuário //
 let nome = prompt("Digite seu nome: ").toLowerCase()
 let idade = parseInt(prompt("Digite sua idade: "))
-
+let senha = prompt("Digite uma senha: ").toLowerCase()
 // verificação da idade//
 if (idade >= 13 && idade < 100) {
-let senha = prompt("Digite uma senha: ").toLowerCase()
        alert("Cadastro realizado com sucesso!")
 let confnome = prompt("Digite o nome do seu cadastro: ").toLowerCase()
 let confsenha = prompt("Digite a senha do seu cadastro: ").toLowerCase()
