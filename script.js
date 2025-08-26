fd31c7afe390bbe0c0c2ddf3bfdf830627b797e7
Keirryson Raffael Lima Soares <keirryson.soares@aluno.ce.gov.br>
2025-08-26 09:36:38 -0300

    modificado o Script

--- script.js
+++ script.js
@@ -5,10 +5,7 @@ let idade = parseInt(prompt("Digite sua idade: "))
 // verificação da idade//
 if (idade >= 13) {
     let senha = prompt("Digite uma senha: ")
-} else{
-    alert("Você não pode se cadastrar. Idade mínima: 13 anos.")
-}
-alert("Cadastro realizado com sucesso!")
+    alert("Cadastro realizado com sucesso!")
 // login do usuário //
 let confnome = prompt("Digite o nome do seu cadastro: ")
 let confsenha = prompt("Digite a senha do seu cadastro: ")
@@ -16,4 +13,7 @@ let confsenha = prompt("Digite a senha do seu cadastro: ")
 if (confnome === nome && confsenha === senha) {
     alert(`Login realizado com sucesso! Bem-vindo, ${confnome}.
 `)
+}
+} else{
+    alert("Você não pode se cadastrar. Idade mínima: 13 anos.")
 }
